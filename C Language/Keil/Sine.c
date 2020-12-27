cf62a8e669d417a51c262240e0608d5277396772
Shruti Murarka <shrutimurarka5@gmail.com>
2020-12-28 03:43:51 +0530

    Create Sine.c

---
+++ C Language/Keil/Sine.c
@@ -0,0 +1,16 @@
+//Sine wave without using array
+#include<reg51.h>
+#include<math.h>
+
+void main()
+{
+    while(1)
+	{
+	int i;
+	double r;
+//Producing Sine Wave
+	for (i=0; i<=360; i++)
+	{r = sin(2*3.1415*i/360);} 
+		  
+	}
+}
